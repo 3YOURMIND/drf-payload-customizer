1dcb7baa0bba426cd65ad9f718f47b7ff7758753
Tony Thomas <01tonythomas@gmail.com>
2018-10-16 10:55:40 +0200

    Revert moving tests outside codebase

--- drf_payload_customizer/tests/test_app/tests/tests.py
+++ drf_payload_customizer/tests/test_app/tests/tests.py
@@ -4,7 +4,6 @@ from django.test import TestCase
 from django.utils.six import BytesIO
 
 
-from rest_framework import serializers
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
 from rest_framework.serializers import ModelSerializer
@@ -17,30 +16,10 @@ class TestModel(models.Model):
     param_b = models.IntegerField()
 
 
-class CustomTestModelSerializer(PayloadCustomizationMixin,
-                                serializers.ModelSerializer):
+class CustomTestModelSerializer(PayloadCustomizationMixin, ModelSerializer):
     class Meta:
         model = TestModel
         fields = ('parama', 'param_b',)
-        field_mappings = {
-            'parama': 'paramA'
-        }
-
-
-class NestedTestModelSerializer(PayloadConverterMixin,
-                                serializers.ModelSerializer):
-    param_c = serializers.SerializerMethodField()
-
-    def get_param_c(self, instance):
-        return {
-            'key': 'value',
-            'key_snake': 'value_snake',
-            'keyCamel': 'valueCamel'
-        }
-
-    class Meta:
-        model = TestModel
-        fields = ('parama', 'param_b', 'param_c',)
         field_mappings = {
             'parama': 'paramA'
         }
@@ -68,14 +47,3 @@ class PayloadConverterMixinTest(TestCase):
         data = JSONParser().parse(stream)
         serializer = CustomTestModelSerializer(data=data)
         self.assertTrue(serializer.is_valid())
-
-    def test_payload_conversion_with_nested_fields(self):
-        test_object = TestModel()
-        test_serializer = NestedTestModelSerializer(test_object)
-        serialized_data = JSONRenderer().render(test_serializer.data)
-        self.assertEquals(
-            {'paramA': '', 'paramB': None,
-             'paramC': {'key': 'value', 'keySnake': 'value_snake',
-                        'keyCamel': 'valueCamel'}},
-            json.loads(serialized_data)
-        )
